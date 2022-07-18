1c61c1fa098128d5a1026660d719ff6f80210648
+ <+>
2022-07-18 18:10:35 -0400

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-// code your solution here
+
+function  superbowlWin(recordArray) {
+    const found = recordArray.find(element => element.result === "W")
+        if(found === undefined) {
+            return undefined
+        }
+        else {
+            return found.year
+        }
+
+}
+
+//const found = array1.find(element => element > 10);
